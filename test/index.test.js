2f18922ccd9eb96c0000eb449abc2a990e6efb1c
MauriMDev <maurimdev@gmail.com>
2025-10-07 23:37:47 -0600

    first commit

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-const index = require('./testindex');
+const hash = require('./testindex');
 
-test('Filter even numbers', () => {
-    const message = "contraseña";
-    const filteredNumbers = index(message);
-    expect(filteredNumbers).toEqual("559a0612917c8c516c7980c38b376cf2bb2387ae1b6944b3b32245ff4f2bad4d5db3811f02cff394fb62fa3feefd03e2eb83ba7bcca72a912bb87ea609c424da");
+test("verificar hash sha256", () => {
+    const mensaje = "Hola, este es un mensaje de prueba.";
+    const hashTest = hash(mensaje);
+    expect(hashTest).toBe("2cc5170cf82fc5e805680ae861f02ddb40c2052668f4223212c9644341dfccd1");
 });
